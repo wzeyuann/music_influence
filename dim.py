116d3dbafb428801b74870adeee29ece102911f7
Harry Xue <wxue@college.harvard.edu>
2018-03-10 00:22:24 -0500

    Pushing Odyssey versions of dim.py and dim_first.py

--- dim.py
+++ dim.py
@@ -5,15 +5,17 @@ from gensim.models.wrappers.dtmmodel import DtmModel
 import numpy as np
 import pandas as pd
 from time import time
+import pickle
 
 # Set path to dtm binary
 dtm_path = "/n/home09/hxue/dtm/dtm/dtm"
-# Get paths to bow directories for each artist
-BOW_DIR = '/n/regal/rush_lab/xue/bow_500/'
 
 # Model settings
-NUM_TOPICS = 5
-MODEL_SAVE_NAME = 'dim_bow500_{}topics_alltracks_songreleasedate'.format(NUM_TOPICS)
+NUM_TOPICS = 10
+BOW_DIM = 1000
+# Get paths to bow directories for each artist
+BOW_DIR = '/n/regal/rush_lab/xue/bow_{}/'.format(BOW_DIM)
+MODEL_SAVE_NAME = 'dim_bow{}_{}topics_alltracks_songreleasedate'.format(BOW_DIM, NUM_TOPICS)
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -37,7 +39,7 @@ for artist_id in os.listdir(BOW_DIR):
         # Check if song year is missing or artist is missing from song df
         if int(artist_id) in ids_in_songs and songs[songs['name_ext'] == song_filename]['year'].iloc[0] != 0:
             # save (artist_id, path, year) tuple
-            bow_path_by_artist.append((int(artist_id), BOW_DIR + artist_id + '/', songs[songs['name_ext'] == song_filename]['year'].iloc[0]))
+            bow_path_by_artist.append((int(artist_id), artist_id + '/' + song, songs[songs['name_ext'] == song_filename]['year'].iloc[0]))
 
 print "Number of songs:", len(bow_path_by_artist)
 
@@ -60,9 +62,9 @@ print time_seq
 
 class BoWCorpus(object):
     def __iter__(self, bow_path_by_artist=bow_path_by_artist):
-        for artist_id, artist_path, year in bow_path_by_artist:
-            # Extract features for first song in the directory
-            bow = np.load(artist_path + os.listdir(artist_path)[0])
+        for artist_id, song_path, year in bow_path_by_artist:
+            # Load features for each song
+            bow = np.load(BOW_DIR + song_path)
             # Convert to sparse encoding
             bow_sparse = [(idx, count) for (idx, count) in enumerate(bow) if count > 0]
             yield bow_sparse
@@ -82,3 +84,7 @@ model = DtmModel(dtm_path,
 model.save(MODEL_SAVE_NAME)
 
 print 'Model fit in', ((time() - start) / 60.) / 60., 'hours'
+
+# Save list of paths
+pickle.dump(bow_path_by_artist, open(MODEL_SAVE_NAME + "bow_paths.pk", "wb" ))
+
--- dim_first.py
+++ dim_first.py
@@ -5,6 +5,7 @@ from gensim.models.wrappers.dtmmodel import DtmModel
 import numpy as np
 import pandas as pd
 from time import time
+import pickle
 
 # Set path to dtm binary
 dtm_path = "/n/home09/hxue/dtm/dtm/dtm"
@@ -12,7 +13,7 @@ dtm_path = "/n/home09/hxue/dtm/dtm/dtm"
 BOW_DIR = '/n/regal/rush_lab/xue/bow_1000/'
 
 # Model settings
-NUM_TOPICS = 10
+NUM_TOPICS = 5
 MODEL_SAVE_NAME = 'dim_bow1000_{}topics_firsttrack_songreleasedate'.format(NUM_TOPICS)
 
 logger = logging.getLogger()
@@ -36,7 +37,7 @@ for artist_id in os.listdir(BOW_DIR):
     # Check if song year is missing or artist is missing from song df
     if int(artist_id) in ids_in_songs and songs[songs['name_ext'] == song_filename]['year'].iloc[0] != 0:
         # save (artist_id, path, year) tuple
-        bow_path_by_artist.append((int(artist_id), BOW_DIR + artist_id + '/', songs[songs['name_ext'] == song_filename]['year'].iloc[0]))
+        bow_path_by_artist.append((int(artist_id), artist_id + '/', songs[songs['name_ext'] == song_filename]['year'].iloc[0]))
 
 print "Number of songs:", len(bow_path_by_artist)
 
@@ -57,11 +58,14 @@ time_seq = [year_counter[key] for key in sorted(year_counter.keys())]
 print "Count of artists per each time slice:"
 print time_seq
 
+# Save list of paths
+pickle.dump(bow_path_by_artist, open(MODEL_SAVE_NAME + "bow_paths.pk", "wb" ))
+
 class BoWCorpus(object):
     def __iter__(self, bow_path_by_artist=bow_path_by_artist):
         for artist_id, artist_path, year in bow_path_by_artist:
             # Extract features for first song in the directory
-            bow = np.load(artist_path + os.listdir(artist_path)[0])
+            bow = np.load(BOW_DIR + artist_path + os.listdir(BOW_DIR + artist_path)[0])
             # Convert to sparse encoding
             bow_sparse = [(idx, count) for (idx, count) in enumerate(bow) if count > 0]
             yield bow_sparse
@@ -81,3 +85,4 @@ model = DtmModel(dtm_path,
 model.save(MODEL_SAVE_NAME)
 
 print 'Model fit in', ((time() - start) / 60.) / 60., 'hours'
+
